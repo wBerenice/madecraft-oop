997d00578448df0fdb47e62121edd86a618b5d21
BereniceSan <sanchezberenice09@gmail.com>
2023-01-29 09:47:54 -0600

    Exercise 02 Harry resolved - 2.Inside Classes

--- 2. Inside Classes/exercises/Harry.java
+++ 2. Inside Classes/exercises/Harry.java
@@ -6,11 +6,11 @@ public class Harry {
 
 	Harry() {
 		System.out.println("making Harry Potter...");
-	}
+	}//constructor (no parameters)
 
 	void castSpell(String spell) {
 		System.out.println("casting spell: " + spell);
-	}
+	}//method castSpell
 
 	void makeInvisible(boolean invisible) {
 		this.cloakOn = invisible;
@@ -19,18 +19,25 @@ public class Harry {
 			System.out.println("Harry is invisible");
 		else
 			System.out.println("Harry is visible");
-	}
+	}//method makeInvisible
 
 	void spyOnSnape() {
 		System.out.println("Harry sees Professor Snape doing nefarious things.");
-	}
+	}//method spyOnSnape
 
 	public static void main(String[] args) {
 		// 1. make harry potter
+		Harry harryPotter = new Harry(); //output: "making Harry Potter..."
+
 		// 2. become invisible
+		harryPotter.makeInvisible(true); //output: "Harry is invisible"
 		// 3. spy on professor snape
+		harryPotter.spyOnSnape(); //output: "Harry sees Professor Snape doing nefarious things."
 		// 4. become visible again
+		harryPotter.makeInvisible(false); //output: "Harry is visible"
+
 		// 5. cast a “stupefy” spell
+		harryPotter.castSpell("stupefy"); //output: "casting spell: stupefy"
 	}
 
 }
